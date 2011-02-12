d43e4c352a7ee2792dd9189d69d7cbc2ae971c6d
Alexander Auritt <alexauritt@gmail.com>
2011-02-12 10:22:21 -0500

    basic empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mathhotspot::Application.config.secret_token = '049157f52a00aa296bc5abf3e475ce7dfb36a67106925df66a25118d4cb6aecc6a9a9a93fe300971cccca0a3d5c998f7ab14fc8f5ed0bfe42d5c2e969f2e29d1'
