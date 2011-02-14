cf4bafa860532fb014518347668660fd8c72ea02
Alexander Auritt <alexauritt@gmail.com>
2011-02-14 13:37:23 -0500

    added MathProblemTemplate.find_replacement(math_problem)
    added placeholder 'replace' link (to google) in worksheet view
    
    fixed seeds.rb
    
    added mocha

--- app/models/math_problem_template.rb
+++ app/models/math_problem_template.rb
@@ -2,4 +2,21 @@ class MathProblemTemplate < ActiveRecord::Base
   belongs_to :lesson
   belongs_to :instructions
   has_many :math_problems
+  
+  def self.find_replacement(math_problem)
+    available_problems = MathProblem.find_all_by_math_problem_template_id(math_problem.math_problem_template.id)
+
+    if available_problems.empty?
+      raise ActiveRecord::RecordNotFound
+      return nil
+    end
+    
+    if (available_problems.count == 1)
+      raise ActiveRecord::RecordNotFound
+      return availble_problems.first
+    end
+    
+    without_original = available_problems.delete_if {|problem| problem == math_problem}
+    without_original[rand(without_original.size)]
+  end
 end
--- test/unit/math_problem_templates_test.rb
+++ test/unit/math_problem_templates_test.rb
@@ -1,8 +1,32 @@
 require 'test_helper'
+require 'mocha'
 
 class MathProblemsTemplateTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  def setup
+    @first_template = MathProblemTemplate.new(:id => 234)
+    @first_problem = MathProblem.new(:math_problem_template => @first_template)
+    @second_problem = MathProblem.new(:math_problem_template => @first_template)    
+  end
+  
+  test "replace math problem returns different problem with same template" do
+    MathProblem.expects(:find_all_by_math_problem_template_id).with(@first_template.id).returns([@first_problem, @second_problem])
+    replacement = MathProblemTemplate.find_replacement(@first_problem)
+    assert_equal(@second_problem, replacement)
+  end
+  
+  test "find_replacement returns nil and raises if specified problem's template can not be found" do
+    MathProblem.expects(:find_all_by_math_problem_template_id).with(@first_template.id).returns([])
+    assert_raise ActiveRecord::RecordNotFound do
+      assert_nil MathProblemTemplate.find_replacement(@first_problem)
+    end
+  end
+  
+  test "find_replacement returns same problem and raises if problem is one of a kind" do
+    MathProblem.expects(:find_all_by_math_problem_template_id).with(@first_template.id).returns([@first_problem])
+    assert_raise ActiveRecord::RecordNotFound do
+      assert_equal(@first_problem, MathProblemTemplate.find_replacement(@first_problem))
+    end
   end
 end
