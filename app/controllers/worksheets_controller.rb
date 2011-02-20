2ed6130049069e97c48770f971301be734abd15b
Alexander Auritt <alexauritt@gmail.com>
2011-02-20 11:48:57 -0500

    added show action and view to worksheet controller

--- app/controllers/worksheets_controller.rb
+++ app/controllers/worksheets_controller.rb
@@ -3,4 +3,10 @@ class WorksheetsController < ApplicationController
     @math_problems = MathProblem.all
     render :template => "worksheets/index.xhtml.erb", :content_type => 'application/xhtml+xml'
   end
+  
+  def show
+    @worksheet = Worksheet.find(params[:id])
+    @math_problems = @worksheet.math_problems
+    render :template => "worksheets/show.xhtml.erb", :content_type => 'application/xhtml+xml'
+  end
 end
