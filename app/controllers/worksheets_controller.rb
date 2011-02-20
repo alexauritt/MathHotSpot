9ea7bec0a2f2d0f10e54fa3eba0e2e002658d95b
Alexander Auritt <alexauritt@gmail.com>
2011-02-20 14:30:33 -0500

    can replace worksheet math problems but new prob goes to end of list

--- app/controllers/worksheets_controller.rb
+++ app/controllers/worksheets_controller.rb
@@ -9,4 +9,15 @@ class WorksheetsController < ApplicationController
     @math_problems = @worksheet.math_problems
     render :template => "worksheets/show.xhtml.erb", :content_type => 'application/xhtml+xml'
   end
+
+  def update
+    @worksheet = Worksheet.find(params[:id])
+    old_problem = MathProblem.find(params[:math_problem_id])
+    new_problem = MathProblemTemplate.find_replacement(old_problem)
+    @worksheet.math_problems.delete old_problem 
+    @worksheet.math_problems << new_problem
+    @worksheet.save
+    @math_problems = @worksheet.math_problems
+    render :template => "worksheets/show.xhtml.erb", :content_type => 'application/xhtml+xml'
+  end
 end
