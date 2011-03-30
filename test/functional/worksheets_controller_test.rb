7652b5ed966d962c95ebfd42de471971a4b1a8e7
Alexander Auritt <alexauritt@gmail.com>
2011-03-30 16:32:31 -0400

    additional tests for worksheet update/replace

--- test/functional/worksheets_controller_test.rb
+++ test/functional/worksheets_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class WorksheetsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  test "update/replace problem failure should display correct error if problem is unique" do
+  test "update/replace problem failure should display correct error msg if problem is unique" do
     worksheet = worksheets(:monomial_worksheet_01)
     unique_problem = worksheet_problems(:monomial_worksheet_unique_problem)
 
@@ -12,4 +12,26 @@ class WorksheetsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:worksheet)
     assert_select 'p.notice', MathHotSpotErrors::Message::UNIQUE
   end
+  
+  test "update/replace error display for no remaining problems of given type" do
+    worksheet = worksheets(:monomial_worksheet_01)
+    second_problem_of_two_on_worksheet = worksheet_problems(:monomial_worksheet_prob_05)
+
+    put(:update, {:id => worksheet.id, :problem_number => second_problem_of_two_on_worksheet.problem_number })
+
+    assert_response :success
+    assert_not_nil assigns(:worksheet)
+    assert_select 'p.notice', MathHotSpotErrors::Message::NONE_REMAINING
+  end
+
+  test "update/replace error display for request replacement of problem that doesn't exist" do
+    worksheet = worksheets(:monomial_worksheet_01)
+
+    put(:update, {:id => worksheet.id, :problem_number => worksheet.worksheet_problems.size + 1 })
+
+    assert_response :success
+    assert_not_nil assigns(:worksheet)
+    assert_select 'p.notice', MathHotSpotErrors::Message::DEFAULT
+  end
+
 end
