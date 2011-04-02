6b0071c704e296b9169864c4c5df329f9df2a717
Alexander Auritt <alexauritt@gmail.com>
2011-04-02 15:21:11 -0400

    adding and refactoring tests

--- test/functional/worksheets_controller_test.rb
+++ test/functional/worksheets_controller_test.rb
@@ -5,21 +5,14 @@ class WorksheetsControllerTest < ActionController::TestCase
   
   test "update/replace changes div content of target problem" do
     worksheet = worksheets(:monomial_worksheet_01)
-    get(:show, {:id => worksheet.id})
-
-
-    initial_state = css_select('div.worksheet div#problem_3')[0]
-    put(:update, {:id => worksheet.id, :problem_number => 3})
-    after_state = css_select('div.worksheet div#problem_3')[0]
-    
-    assert_not_equal initial_state, after_state, "Problem 3 did not change after replace call."
+    assert_problem_changes_on_update(worksheet, 3)
   end
   
   test "update/replace problem failure should display correct error msg if problem is unique" do
     worksheet = worksheets(:monomial_worksheet_01)
     unique_problem = worksheet_problems(:monomial_worksheet_unique_problem)
-  
-    put(:update, {:id => worksheet.id, :problem_number => unique_problem.problem_number })
+
+    assert_problem_does_not_change_on_update(worksheet, unique_problem.problem_number)
   
     assert_response :success
     assert_not_nil assigns(:worksheet)
@@ -30,8 +23,8 @@ class WorksheetsControllerTest < ActionController::TestCase
     worksheet = worksheets(:monomial_worksheet_01)
     second_problem_of_two_on_worksheet = worksheet_problems(:monomial_worksheet_prob_05)
   
-    put(:update, {:id => worksheet.id, :problem_number => second_problem_of_two_on_worksheet.problem_number })
-  
+    assert_problem_does_not_change_on_update(worksheet, second_problem_of_two_on_worksheet.problem_number)  
+
     assert_response :success
     assert_not_nil assigns(:worksheet)
     assert_select 'p.notice', MathHotSpotErrors::Message::NONE_REMAINING
@@ -39,12 +32,34 @@ class WorksheetsControllerTest < ActionController::TestCase
   
   test "update/replace error display for request replacement of problem that doesn't exist" do
     worksheet = worksheets(:monomial_worksheet_01)
-  
-    put(:update, {:id => worksheet.id, :problem_number => worksheet.worksheet_problems.size + 1 })
+    
+    nonexistent_problem_number = worksheet.worksheet_problems.size + 1
+    assert_problem_does_not_change_on_update(worksheet, nonexistent_problem_number)  
   
     assert_response :success
     assert_not_nil assigns(:worksheet)
     assert_select 'p.notice', MathHotSpotErrors::Message::DEFAULT
   end
+  
+  private
+  
+  def assert_problem_changes_on_update(worksheet, problem_number)
+    compare_problem_before_and_after_update(worksheet, problem_number, false)
+  end
 
+  def assert_problem_does_not_change_on_update(worksheet, problem_number)
+    compare_problem_before_and_after_update(worksheet, problem_number, true)
+  end
+  
+  def compare_problem_before_and_after_update(worksheet, problem_number, should_be_equal)
+    get(:show, {:id => worksheet.id})
+    initial_state = css_select("div.worksheet div#problem_#{problem_number}")[0]
+    put(:update, {:id => worksheet.id, :problem_number => problem_number})
+    after_state = css_select("div.worksheet div#problem_#{problem_number}")[0]
+    if (should_be_equal)
+      assert_equal initial_state, after_state, "Problem #{problem_number} did not change after replace call."
+    else
+      assert_not_equal initial_state, after_state, "Problem #{problem_number} change after replace call but should have remained unchanged."
+    end
+  end
 end
