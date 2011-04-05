e3cda4a95f13a63be2b4dd4ac070955c8128fcac
Alexander Auritt <alexauritt@gmail.com>
2011-04-05 18:31:43 -0400

    math_problems#index uses mathjax / html5, no xhtml

--- app/controllers/math_problems_controller.rb
+++ app/controllers/math_problems_controller.rb
@@ -1,6 +1,5 @@
 class MathProblemsController < ApplicationController
   def index
     @math_problems = MathProblem.all
-    render :template => "math_problems/index.xhtml.erb", :layout => "application.xhtml.erb", :content_type => 'application/xhtml+xml'
   end
 end
