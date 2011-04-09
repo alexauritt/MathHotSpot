7728687cdbc33780073eea8d381976762b8b5821
Alexander Auritt <alexauritt@gmail.com>
2011-04-08 23:07:27 -0400

    added show, create, new actions, views, and routes to MathProblemController

--- app/controllers/math_problems_controller.rb
+++ app/controllers/math_problems_controller.rb
@@ -1,5 +1,25 @@
 class MathProblemsController < ApplicationController
   def index
     @problem_groups = MathProblem.group_all_by_template
-  end  
+  end
+  
+  def show
+    @math_problem = MathProblem.find(params[:id])
+  end
+  
+  def create
+    @math_problem = MathProblem.new(params[:math_problem])
+    respond_to do |format|
+      if @math_problem.save
+        format.html { redirect_to(@math_problem, :notice => 'Math Problem was successfully created.') }
+      else
+        format.html { render :action => "new" }
+      end
+    end
+  end
+  
+  def new
+    @math_problem = MathProblem.new
+  end
+    
 end
--- test/functional/math_problems_controller_test.rb
+++ test/functional/math_problems_controller_test.rb
@@ -2,7 +2,31 @@ require 'test_helper'
 
 class MathProblemsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @math_problem = math_problems(:simple_monomial_problem_01)
+    @request.env["HTTP_AUTHORIZATION"] = "Basic " + Base64::encode64("foo:bar")    
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:problem_groups)
+  end
+  
+  test "should show math problem" do
+    get :show, :id => @math_problem.to_param
+    assert_response :success
+  end
+  
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should create math problem" do
+    assert_difference('MathProblem.count') do
+      post :create, :math_problem => @math_problem.attributes
+    end
+    assert_redirected_to math_problem_path(assigns(:math_problem))
   end
 end
