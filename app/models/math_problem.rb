83cbc19c6e12fa12587275ae3da8915667265ed9
Alexander Auritt <alexauritt@gmail.com>
2011-04-11 11:48:49 -0400

    MathProblem strips_excess_tags from MathType paste results before validating
    MathJax config change: display MathMl or LaTeX

--- app/models/math_problem.rb
+++ app/models/math_problem.rb
@@ -2,6 +2,7 @@ class MathProblem < ActiveRecord::Base
   has_many :worksheets, :through => :worksheet_problems
   belongs_to :math_problem_template
   has_one :instruction, :through => :math_problem_template
+  before_validation :strip_excess_tags
   
   def self.grouped_problems
     where("math_problem_template_id").order("math_problem_template_id")
@@ -20,5 +21,14 @@ class MathProblem < ActiveRecord::Base
     probs = grouped_problems.chunk {|prob| prob.math_problem_template }.each {|template, cur_group| @problem_groups << cur_group }
     @problem_groups
   end
-   
+  
+  private
+  
+  def strip_excess_tags
+    question_markup.gsub!(/\n/,"")
+    question_markup.gsub!(/<\/?math[^>]*>|<\/?semantics>|<annotation.*annotation>/,"")
+    question_markup.gsub!(/>\s*</,"><")
+    question_markup.strip!
+  end
+     
 end
--- test/unit/math_problems_test.rb
+++ test/unit/math_problems_test.rb
@@ -39,6 +39,38 @@ class MathProblemTest < ActiveSupport::TestCase
     assert MathProblem.new.display_mode?
   end
   
+  test "strip_excess_tags" do
+    input_from_math_type = "<math display='block'><semantics><mrow>        <msqrt><mrow><msup><mi>a</mi>      <mn>2</mn></msup>      <mo>+</mo><msup><mi>b</mi><mn>2</mn></msup></mrow></msqrt><mo>+</mo><mtext>&#x200B;</mtext><mfrac><mrow><mi>n</mi><mo>!</mo></mrow><mrow><mi>r</mi><mo>!</mo><mrow><mo>(</mo><mrow><mi>n</mi><mo>&#x2212;</mo><mi>r</mi></mrow><mo>)</mo></mrow><mo>!</mo></mrow></mfrac></mrow><annotation encoding='MathType-MTEF'>     MathType@MTEF@5@5@+=faaagaart1ev2aaaKnaaaaWenf2ys9wBH5garuavP1wzZbqedmvETj2BSbqefm0B1jxALjharqqtubsr4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8FesqqrFfpeea0xe9Lq=Jc9vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=xfr=xb9Gqpi0dc9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbaWaaOaaaeaacaWGHbWaaWbaaSqabeaacaaIYaaaaOGaey4kaSIaamOyamaaCaaaleqabaGaaGOmaaaaaeqaaOGaey4kaSIaaGzaVpaalaaabaGaamOBaiaacgcaaeaacaWGYbGaaiyiamaabmaabaGaamOBaiabgkHiTiaadkhaaiaawIcacaGLPaaacaGGHaaaaaaa@3D6C@</annotation></semantics></math>"
+    all_striped_down = "<mrow><msqrt><mrow><msup><mi>a</mi><mn>2</mn></msup><mo>+</mo><msup><mi>b</mi><mn>2</mn></msup></mrow></msqrt><mo>+</mo><mtext>&#x200B;</mtext><mfrac><mrow><mi>n</mi><mo>!</mo></mrow><mrow><mi>r</mi><mo>!</mo><mrow><mo>(</mo><mrow><mi>n</mi><mo>&#x2212;</mo><mi>r</mi></mrow><mo>)</mo></mrow><mo>!</mo></mrow></mfrac></mrow>"
+    problem = MathProblem.new(:question_markup => input_from_math_type)
+    problem.send(:strip_excess_tags)
+    
+    assert_equal all_striped_down, problem.question_markup
+  end
+  
+  test "strip_excess_tags removes newlines" do
+    input_from_math_type = "<math display='block'>
+     <semantics>
+      <mrow>
+       <mfrac>
+        <mi>x</mi>
+        <mn>2</mn>
+       </mfrac>
+
+      </mrow>
+     <annotation encoding='MathType-MTEF'>
+     MathType@MTEF@5@5@+=faaagaart1ev2aaaKnaaaaWenf2ys9wBH5garuavP1wzZbqedmvETj2BSbqefm0B1jxALjharqqtubsr4rNCHbGeaGqiVu0Je9sqqrpepC0xbbL8FesqqrFfpeea0xe9Lq=Jc9vqaqpepm0xbba9pwe9Q8fs0=yqaqpepae9pg0FirpepeKkFr0xfr=xfr=xb9Gqpi0dc9adbaqaaeGaciGaaiaabeqaamaabaabaaGcbaWaaSaaaeaacaWG4baabaGaaGOmaaaaaaa@2FDB@</annotation>
+     </semantics>
+    </math>
+    "
+    all_striped_down = "<mrow><mfrac><mi>x</mi><mn>2</mn></mfrac></mrow>"
+    problem = MathProblem.new(:question_markup => input_from_math_type)
+    problem.send(:strip_excess_tags)
+    
+    assert_equal all_striped_down, problem.question_markup
+  end
+  
+  private
   def stub_math_problem_order_to_return_sorted_list(ordered_problem_list)
     MathProblem.stubs(:grouped_problems).returns(ordered_problem_list)
   end  
