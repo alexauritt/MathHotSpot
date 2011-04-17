7aa999414cb317716ab5d714fd263b9ab39a2d23
Alexander Auritt <alexauritt@gmail.com>
2011-04-17 11:00:01 -0400

    refactoring tests

--- test/unit/worksheet_test.rb
+++ test/unit/worksheet_test.rb
@@ -34,7 +34,7 @@ class WorksheetTest < ActiveSupport::TestCase
     assert_equal true, @worksheet.replace_problem(2)
   end
 
-  test "replace_problems actually replaces expected problem" do
+  test "replace_problems replaces expected problem" do
     create_mock_worksheet_problems_for(@worksheet, { :count => 3 })
     
     worksheet_problems = @worksheet.worksheet_problems
@@ -47,32 +47,6 @@ class WorksheetTest < ActiveSupport::TestCase
     @worksheet.replace_problem 2
     
     assert_equal replacement_problem, @worksheet.worksheet_problems[1].math_problem
-  end
-  
-  test "ids_of_similar_problems_on_worksheet (helper method)" do
-    mp1 = mock
-    mp2 = mock
-    mp3 = mock
-  
-    wp1 = mock
-    wp1.stubs(:math_problem => mp1)
-    wp2 = mock
-    wp2.stubs(:math_problem => mp2)
-    wp3 = mock
-    wp3.stubs(:math_problem => mp3)
-    
-    
-    mp_temp1 = mock
-    mp_temp2 = mock
-  
-    mp1.stubs(:id => 123, :math_problem_template => mp_temp1)
-    mp2.stubs(:id => 456, :math_problem_template => mp_temp1)
-    mp3.stubs(:id => 789, :math_problem_template => mp_temp2)        
-    
-    @sheet = Worksheet.new
-    @sheet.stubs(:math_problems).returns([mp1,mp2,mp3])       # should not have to stub these active record refs. mocha bug?
-    @sheet.stubs(:worksheet_problems).returns([wp1,wp2,wp3])  # should not have to stub these active record refs. mocha bug?
-    assert_equal [mp2.id], @sheet.send(:ids_of_similar_problems_on_worksheet, mp1)
   end
   
   test "similar problems on worksheet" do
@@ -91,52 +65,25 @@ class WorksheetTest < ActiveSupport::TestCase
     assert_equal [prob2, prob4], worksheet.send(:similar_problems_on_worksheet, prob1)
   end  
 
-  test "replace_problem does not replace with other problems of same type on worksheet" do
-    w = Worksheet.new
-    mpt1 = MathProblemTemplate.new(:id => 8976)
-    mpt2 = MathProblemTemplate.new(:id => 5332)
-
-    wp1 = new_worksheet_problem_of_type(mpt1, 1)
-    wp2 = new_worksheet_problem_of_type(mpt1, 2)
-    
-    wp3 = new_worksheet_problem_of_type(mpt2, 3)
-    
-    replacement = MathProblem.new(:math_problem_template => mpt1, :id => 9994)
-
-    w.worksheet_problems = [wp1, wp2, wp3]
-    w.math_problems = w.worksheet_problems.map {|p| p.math_problem }
-
-    assert_not_equal wp2.math_problem, replacement
-    assert_equal wp2.math_problem, w.worksheet_problems[1].math_problem
-
-    # MathProblemTemplate.expects(:find_replacement).with(wp2.math_problem, {:exclude => [wp1.math_problem]}).returns(replacement)
-    MathProblemTemplate.stubs(:find_replacement).returns(replacement)
-    w.replace_problem(2)
-
-    assert_equal replacement, w.worksheet_problems[1].math_problem
+  def set_same_template_for_every_problem_on_worksheet_except_number(problem_number)
+    template1, template2 = MathProblemTemplate.new, MathProblemTemplate.new
+    @worksheet.worksheet_problems.each_with_index do |wp, index|
+      template = (index == problem_number - 1) ? template1 : template2 
+      wp.stubs(:math_problem_template).returns(template)
+    end
   end
-  
+
   test "replace_problem excludes similar problems on same worksheet" do
-    type1 = mock
-    w = Worksheet.new
-
-    wp1 = w.worksheet_problems.build
-    wp1.stubs(:math_problem_template).returns(type1)
-    mp1 = mock
-    wp1.stubs(:math_problem).returns(mp1)
-
-    wp2 = w.worksheet_problems.build
-    wp2.stubs(:math_problem_template).returns(type1)
-    mp2 = mock
-    wp2.stubs(:math_problem).returns(mp2)
-    
-    MathProblemTemplate.expects(:find_replacement).with(mp1, :exclude => [mp2])
-    
-    w.replace_problem 1
+    create_mock_worksheet_problems_for(@worksheet, {:count => 6})
+    set_same_template_for_every_problem_on_worksheet_except_number(5)
+    first_problem = @worksheet.worksheet_problems[0].math_problem
+    math_problems_similar_to_first = @worksheet.worksheet_problems[1..3].map {|wp| wp.math_problem }
+    math_problems_similar_to_first << @worksheet.worksheet_problems[5].math_problem
+    MathProblemTemplate.expects(:find_replacement).with(first_problem, :exclude => math_problems_similar_to_first)
+        
+    @worksheet.replace_problem 1
   end
 
-
-    
   private
 
   def assert_all_have_same_instructions(problem_group)
@@ -151,9 +98,8 @@ class WorksheetTest < ActiveSupport::TestCase
   
   def create_mock_worksheet_problems_for(worksheet, options = { :count => 1 })
     options[:count].times do |i|
-      worksheet_problem = WorksheetProblem.new(:problem_number => i+1)
-      worksheet_problem.math_problem = MathProblem.new(:question_markup => "some markup goes here #{i}")
-      worksheet.worksheet_problems << worksheet_problem
+      worksheet_problem = worksheet.worksheet_problems.build
+      worksheet_problem.build_math_problem(:question_markup => "this is some question markup #{i}")
     end
   end
   
