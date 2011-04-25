45d7769bb38b66443fd9d032d5254c0f74790a53
Alexander Auritt <alexauritt@gmail.com>
2011-04-25 10:00:06 -0400

    adding links for improved navigation

--- app/controllers/problem_levels_controller.rb
+++ app/controllers/problem_levels_controller.rb
@@ -1,5 +1,6 @@
 class ProblemLevelsController < ApplicationController
   def show
-    @math_problems = ProblemLevel.find(params[:id]).math_problems
+    @problem_level = ProblemLevel.find(params[:id])
+    @math_problems = @problem_level.math_problems
   end
 end
