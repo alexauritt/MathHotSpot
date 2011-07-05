9afa1b92632d4aca0f71e79490f4ed6dd69e0f70
Alexander Auritt <alexauritt@gmail.com>
2011-07-05 17:19:13 +0200

    Lessons index (should be dashboard or something) displays 'My Problem Types'

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -12,7 +12,8 @@ class LessonsController < ApplicationController
   end
   
   def index
-    @lessons = Lesson.find_all_by_owner_id(current_user.id) 
+    @lessons = Lesson.find_all_by_owner_id(current_user)
+    @problem_types = ProblemType.find_all_by_owner_id(current_user)
   end
 
 end
