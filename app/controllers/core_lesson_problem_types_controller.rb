903bbedaaf67823b541b0cf56dda3438e00d6f29
Alexander Auritt <alexauritt@gmail.com>
2011-07-20 13:26:47 +0200

    more testing and more cautious redirection for CoreLessonProblemType controlelr

--- app/controllers/core_lesson_problem_types_controller.rb
+++ app/controllers/core_lesson_problem_types_controller.rb
@@ -1,7 +1,12 @@
 class CoreLessonProblemTypesController < ApplicationController
   def create
     @core = CoreLessonProblemType.new(params[:core_lesson_problem_type])
-    @core.save
-    redirect_to lesson_path(@core.lesson)
+    message = @core.save ? "Successfully added ProblemType to Lesson." : @core.errors
+    if @core.lesson
+      redirect_to(lesson_path(@core.lesson), :notice => "Unable to add that problem type to this lesson.")
+    else
+      Rails.logger.error("Attempt to add problem type to non-existent lesson")
+      redirect_to(search_problem_types_path, :notice => "Unable to add problem type.")
+    end
   end
 end
--- test/functional/core_lesson_problem_types_controller_test.rb
+++ test/functional/core_lesson_problem_types_controller_test.rb
@@ -2,29 +2,25 @@ require 'test_helper'
 
 class CoreLessonProblemTypesControllerTest < AuthenticatingControllerTestCase
   # Replace this with your real tests.
+  def setup
+    @lesson = Factory.create(:lesson, :owner => users(:testuser))
+    @problem_type = Factory.create(:problem_type)
+    @attributes = Factory.attributes_for(:core_lesson_problem_type, :lesson => @lesson, :problem_type => @problem_type)
+  end
+
   test "create" do
-    lesson = Factory.create(:lesson, :owner => users(:testuser) )
-    problem_type = Factory.create(:problem_type)
-    core_lesson_problem_type = Factory.build(:core_lesson_problem_type, :lesson => lesson, :problem_type => problem_type)
     
     assert_difference('CoreLessonProblemType.count') do
-      post :create, :core_lesson_problem_type => core_lesson_problem_type.attributes
+      post :create, :core_lesson_problem_type => @attributes
     end
     
-    assert_redirected_to lesson_path(lesson)
-    
-    pending "user factory girl attr_for"
+    assert_redirected_to lesson_path(@lesson)
   end
   
-  test "create fails gracefully when problem type is already in lesson" do
-    pending "to do"
-  end
-  
-  test "create fails gracefully when lesson id is invalid" do
-    pending "to do"
-  end
-  
-  test "create fails gracefully when problem_type id is invalid" do
-    pending "to do"
+  test "create fails gracefully when problem type already present in lesson" do
+    core_already_in_db = Factory.create(:core_lesson_problem_type, :lesson => @lesson, :problem_type => @problem_type)
+    assert_no_difference('CoreLessonProblemType.count') do
+      post :create, :core_lesson_problem_type => core_already_in_db.attributes
+    end
   end
 end
