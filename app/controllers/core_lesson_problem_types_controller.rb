c58f3a5a40df3dc8502a6e8e72f915f861c564c6
Alexander Auritt <alexauritt@gmail.com>
2011-07-21 17:52:27 +0200

    fixed message for adding pt to lesson
    readded add pt to lesson button
    
    minor styling tweaks

--- app/controllers/core_lesson_problem_types_controller.rb
+++ app/controllers/core_lesson_problem_types_controller.rb
@@ -3,7 +3,7 @@ class CoreLessonProblemTypesController < ApplicationController
     @core = CoreLessonProblemType.new(params[:core_lesson_problem_type])
     message = @core.save ? "Successfully added ProblemType to Lesson." : @core.errors
     if @core.lesson
-      redirect_to(lesson_path(@core.lesson), :notice => "Unable to add that problem type to this lesson.")
+      redirect_to(lesson_path(@core.lesson), :notice => message )
     else
       Rails.logger.error("Attempt to add problem type to non-existent lesson")
       redirect_to(search_problem_types_path, :notice => "Unable to add problem type.")
