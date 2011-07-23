272c0f77d8b2de72e97cb8c3a5f8389e672a8465
Alexander Auritt <alexauritt@gmail.com>
2011-07-23 20:20:40 +0200

    pt::search#show results nested in current_lesson if specified in session

--- app/controllers/problem_types/search_controller.rb
+++ app/controllers/problem_types/search_controller.rb
@@ -13,6 +13,16 @@ class ProblemTypes::SearchController < ApplicationController
   end
     
   def show
+    begin
+      @current_lesson = session[:current_lesson_id] ? Lesson.find(session[:current_lesson_id]) : nil
+    rescue ActiveRecord::RecordNotFound
+      session[:current_lesson_id] = nil
+    end
+        
+    @search = params[:search]
     @problem_types = ProblemType.search(params[:search])
+    
+    render "show_with_current_lesson" if @current_lesson
   end
+  
 end
--- test/functional/problem_types/search_controller_test.rb
+++ test/functional/problem_types/search_controller_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class ProblemTypes::SearchControllerTest < AuthenticatingControllerTestCase
   # Replace this with your real tests.
+  
+  setup do
+    @problem_type = Factory.build(:problem_type)
+  end
+  
   test "index without current_lesson set in session returns simple problem type search" do
     get :new
     
@@ -48,8 +53,43 @@ class ProblemTypes::SearchControllerTest < AuthenticatingControllerTestCase
     get :show, :search => "Monomial Fraction"
     assert_response :success
     assert assigns(:problem_types).include?(problem_types(:dividing_monomials_problem_type))
-  end  
+  end
   
+  test "show search results displays current lesson when valid current_lesson_id is set in session" do
+    lesson = Factory.build(:lesson, :id => 234234)
+    Lesson.expects(:find).with(anything).returns(lesson)
+    ProblemType.expects(:search).with(anything).returns([@problem_type])
+    
+    get :show, {:search => "Monomial Fraction"}, authenticated_session_with({'current_lesson_id' => lesson.id})
+    
+    assert_response :success
+    assert_equal lesson, assigns(:current_lesson), "Current lesson not found as expected"
+    
+    assert_current_lesson_displayed_in_view
+    assert assigns(:problem_types).include?(@problem_type)
+  end
+  
+  test "show search results displays simple problem type search when invalid current_lesson_id is set in session" do
+    ProblemType.expects(:search).with(anything).returns([@problem_type])
+    get_search_results_with_invalid_current_lesson_id_in_session!
+      
+    assert_response :success
+
+    assert_current_lesson_displayed_in_view false
+    assert assigns(:problem_types).include?(@problem_type)
+  end
+  
+  test "no errors in show search results when current_lesson_id is invalid" do
+    assert_nothing_raised do
+      get_search_results_with_invalid_current_lesson_id_in_session!
+    end
+  end
+  
+  test "show search results clears current_lesson_id in session when said id is invalid" do
+    get_new_search_with_invalid_current_lesson_id_in_session!
+    assert_nil session[:current_lesson_id]
+  end  
+    
   private  
 
   def get_new_search_with_invalid_current_lesson_id_in_session!
@@ -58,4 +98,9 @@ class ProblemTypes::SearchControllerTest < AuthenticatingControllerTestCase
     get :new, {}, authenticated_session_with({'current_lesson_id' => invalid_id})
   end
   
+  def get_search_results_with_invalid_current_lesson_id_in_session!
+    invalid_id = 234234
+    Lesson.expects(:find).with(invalid_id).raises(ActiveRecord::RecordNotFound)
+    get :show, {:search => "for something"}, authenticated_session_with({'current_lesson_id' => invalid_id})
+  end
 end
