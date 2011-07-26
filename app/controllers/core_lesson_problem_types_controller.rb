f5724347ee4b6d3f2c8bc18f72158b6105d6578a
Alexander Auritt <alexauritt@gmail.com>
2011-07-26 10:09:36 +0200

    improved error message for failure to add pt to lesson

--- app/controllers/core_lesson_problem_types_controller.rb
+++ app/controllers/core_lesson_problem_types_controller.rb
@@ -1,12 +1,12 @@
 class CoreLessonProblemTypesController < ApplicationController
   def create
     @core = CoreLessonProblemType.new(params[:core_lesson_problem_type])
-    message = @core.save ? "Successfully added ProblemType to Lesson." : @core.errors
+    message = @core.save ? "Successfully added ProblemType to Lesson." : "Unable to add Problem Type to Lesson"
     if @core.lesson
       redirect_to(lesson_path(@core.lesson), :notice => message )
     else
       Rails.logger.error("Attempt to add problem type to non-existent lesson")
-      redirect_to(new_problem_types_search_path, :notice => "Unable to add problem type.")
+      redirect_to(new_problem_types_search_path, :notice => message)
     end
   end
 end
