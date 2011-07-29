57550d299539c4bdae324fbc9242ea067e91f6f7
Alexander Auritt <alexauritt@gmail.com>
2011-07-29 13:24:17 +0200

    MathProblemYamlTextCreator produces yaml text for each problem

--- lib/content_generation/math_problem_yaml_text_creator.rb
+++ lib/content_generation/math_problem_yaml_text_creator.rb
@@ -1,5 +1,34 @@
-class MathProblemYamlTextCreator
-  def self.doit
-    50
+class MathProblemYamlTextCreator  
+  def self.problem_yaml_text(problem_info_hash)
+    unless valid_hash? problem_info_hash
+      raise ArgumentError
+    end
+    start_and_prob_level = "-\n  problem_level_id: #{problem_info_hash[:problem_level_id]}\n"
+
+    markups = ""
+    problem_info_hash[:markup_templates].keys.each do |key|
+      raw_markup = problem_info_hash[:markup_templates][key]
+      problem_info_hash[:values].each_pair do |key,value|
+        raw_markup.gsub!("@#{key}", "#{value}")
+      end
+      markups.concat("  #{key.to_s.concat('_markup')}: #{raw_markup}\n")
+    end
+    start_and_prob_level.concat(markups).concat("\n")
   end
+  
+  private
+  def self.valid_hash?(hash)
+    answer = true
+    hash[:values].keys.each do |key|
+      hash[:values].keys.each do |cur_key|
+        key_str = key.to_s
+        cur_key_str = cur_key.to_s
+        if (key_str != cur_key_str) && (cur_key_str.include?(key_str) || (key_str.include?(cur_key_str)))
+          answer = false
+        end
+      end
+    end
+    answer
+  end
+  
 end
--- test/lib/content_generation/math_problem_yaml_text_creator_test.rb
+++ test/lib/content_generation/math_problem_yaml_text_creator_test.rb
@@ -2,11 +2,53 @@ require 'test/unit'
 require_relative '../../../lib/content_generation/math_problem_yaml_text_creator'
 
 class MathProblemYamlTextCreatorTest < Test::Unit::TestCase
-  def test_somthing
-    assert true
+
+  def setup
+    @problem_info = {
+      :problem_level_id => 5, 
+      :markup_templates => {
+        :question => "something @sub_me and also @sub_this_one_too",
+        :answer => "you sohuld @replace_this and also @replace_this too"
+      },
+      :values => {
+        :sub_me => 'should go here', :sub_this_one_too => 'something should go here too',
+        :replace_this => 'get rid of this'
+      }
+    }
+    @expected_yaml = "-\n  problem_level_id: 5\n  question_markup: something should go here and also something should go here too\n  answer_markup: you sohuld get rid of this and also get rid of this too\n\n"
+  end
+
+  def test_problem_yaml_includes_problem_level
+    returned_yaml = MathProblemYamlTextCreator.problem_yaml_text(@problem_info)
+    assert returned_yaml.include?("-\n  problem_level_id: 5")
+  end
+    
+  def test_problem_yaml_includes_question_and_answer_markup_keys
+    returned_yaml = MathProblemYamlTextCreator.problem_yaml_text(@problem_info)
+    assert returned_yaml.include?("  question_markup:")
+    assert returned_yaml.include?("  answer_markup:")
   end
   
-  def test_no_no
-    assert_equal 50, MathProblemYamlTextCreator.doit
+  def test_problem_yaml_includes_question_markup_with_actual_markup
+    expected_question_markup = "  question_markup: something should go here and also something should go here too\n"
+
+    returned_yaml = MathProblemYamlTextCreator.problem_yaml_text(@problem_info)
+
+    assert returned_yaml.include?(expected_question_markup), "missing expected question markup, instead got: #{returned_yaml}"
+  end
+  
+  def test_problem_yaml_should_raise_if_one_key_subsumes_the_other
+    info_with_bad_values = {:problem_level_id => 4, :markup_templates => {:first => "@dad and @granddad"},
+      :values => {
+        :dad => "bad value because granddad has 'dad' in it", 
+        :granddad => "I am the subsumer!"
+      }}
+    assert_raise ArgumentError do
+      MathProblemYamlTextCreator.problem_yaml_text(info_with_bad_values)
+    end    
+  end
+  
+  def test_problem_yaml
+    assert_equal @expected_yaml, MathProblemYamlTextCreator.problem_yaml_text(@problem_info)
   end
 end
