233cc747416f286b67e36d6c81000747cdf6f350
Alexander Auritt <alexauritt@gmail.com>
2011-08-10 18:56:23 +0200

    using random variables in simp eq one var add level 1

--- script/content_generation/algebra_I/basic_equations_and_problems/simp_eq_one_var_add_level_1.rb
+++ script/content_generation/algebra_I/basic_equations_and_problems/simp_eq_one_var_add_level_1.rb
@@ -1,18 +1,37 @@
 require "#{Rails.root.to_s}/lib/content_generation/content_generation_helper"
 
 #produce values
+
+def random_variable
+  val = rand(10)
+  case val
+  when 0..3
+    'x'
+  when 4..5
+    'y'
+  when 6
+    'm'
+  when 7
+    'k'
+  when 8
+    'w'
+  when 9
+    'a'
+  end
+end
+
 values_list = []
-10.times do |left_val|
-  Array(left_val .. 9).each do |right_val|
-    values_list << {left: left_val, right: right_val, solution: right_val - left_val }
+10.times do |addend_val|
+  Array(addend_val .. 9).each do |sum_val|
+    values_list << {variable: random_variable, addend: addend_val, sum: sum_val, solution: sum_val - addend_val }
   end
 end
 
 args = {
     :problem_type_title => "Simple Equations: One Variable, Addition",
     :problem_level_number => 1,
-    :question_template => ['\[x + @left = @right\]', '\[@left + x = @right\]', '\[@right = x + @left\]', '\[@right = @left + x\]'],
-    :answer_template => '\[x = @solution\]',
+    :question_template => ['\[@variable + @addend = @sum\]', '\[@addend + @variable = @sum\]', '\[@sum = @variable + @addend\]', '\[@sum = @addend + @variable\]'],
+    :answer_template => ['\[@variable = @solution\]', '\[@variable = @solution\]', '\[@solution = @variable\]', '\[@solution = @variable\]'],
     :values_list => values_list
 }
 builder = MathYamlBuilder.new(args)
