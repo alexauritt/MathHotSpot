642714617727fe4e1abc8750f16962dd36b9450e
Alexander Auritt <alexauritt@gmail.com>
2011-08-16 12:02:17 +0200

    worksheets numeration improved

--- app/helpers/worksheets_helper.rb
+++ app/helpers/worksheets_helper.rb
@@ -1,7 +1,11 @@
 module WorksheetsHelper
-  def problem_links_if_editable(worksheet, problem_number, editable)
+  def display_problem_number(worksheet_problem)
+    "#{worksheet_problem.problem_number})"
+  end
+  
+  def problem_links_if_editable(worksheet_problem, editable)
     if editable
-      render :partial => "worksheet_problems/worksheet_problem_links", :locals => { :worksheet => worksheet, :problem_number => problem_number }
+      render :partial => "worksheet_problems/worksheet_problem_links", :locals => { :worksheet_problem => worksheet_problem }
     end
   end  
 end
--- test/unit/helpers/worksheets_helper_test.rb
+++ test/unit/helpers/worksheets_helper_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 
 class WorksheetsHelperTest < ActionView::TestCase
+  test "display_problem_number" do
+    wp = WorksheetProblem.new(:problem_number => 50)
+    expected_display = "50)"
+    assert_equal expected_display, display_problem_number(wp)
+  end
 end
