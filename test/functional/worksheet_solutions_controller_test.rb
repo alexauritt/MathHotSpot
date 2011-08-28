fed80c4b4e59d92b93de384fa0fb1ab6bb47e421
Alexander Auritt <alexauritt@gmail.com>
2011-08-28 16:54:52 -0400

    renamed .worksheet_problem css class to worksheet-problem
    added worksheet-title to worksheet#show
    
    footer hidden from page view

--- test/functional/worksheet_solutions_controller_test.rb
+++ test/functional/worksheet_solutions_controller_test.rb
@@ -5,6 +5,6 @@ class WorksheetSolutionsControllerTest < AuthenticatingControllerTestCase
     get :show, :id => worksheets(:monomial_worksheet_01)
     assert assigns(:worksheet)
     assert_response :success
-    assert_select '.worksheet_problem .math_display .answer'
+    assert_select '.worksheet-problem .math_display .answer'
   end  
 end
