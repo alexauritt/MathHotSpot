0a168c8ced4506040ca87869705a176c4d2d5d6d
Alexander Auritt <alexauritt@gmail.com>
2011-08-28 00:52:43 -0400

    worksheets added to dashboard view

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,5 +3,6 @@ class DashboardsController < ApplicationController
   def show
     @lessons = Lesson.find_all_by_owner_id(current_user)
     @problem_types = ProblemType.find_all_by_owner_id(current_user)
+    @worksheets = Worksheet.find_all_by_owner_id(current_user)
   end
 end
