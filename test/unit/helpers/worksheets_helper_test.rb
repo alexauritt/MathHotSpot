8c78935a3096128707c7c0fb717f844e7058e26e
Alexander Auritt <alexauritt@gmail.com>
2011-09-04 20:10:01 -0400

    tweaked and installed problem_group_instruction in worksheet#show

--- test/unit/helpers/worksheets_helper_test.rb
+++ test/unit/helpers/worksheets_helper_test.rb
@@ -12,7 +12,7 @@ class WorksheetsHelperTest < ActionView::TestCase
     mock_prob = MathProblem.new
     mock_prob.stubs(:instruction_description).returns(expected_msg)
     prob_group = Array.new(4) {|i| Factory.build(:worksheet_problem, :problem_number => (i + 15), :math_problem => mock_prob, :worksheet => Worksheet.new) }
-    expected_message = "Problems 15, 16, 17, 18: Solve the damn problem."
+    expected_message = "Problems #15, 16, 17, 18: Solve the damn problem."
     assert_equal expected_message, problem_group_instructions(prob_group)
   end
 end
