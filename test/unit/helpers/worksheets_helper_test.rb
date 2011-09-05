62d664382793a6ce2387001a6fc845eebe615137
Alexander Auritt <alexauritt@gmail.com>
2011-09-04 20:53:05 -0400

    refactored test

--- test/unit/helpers/worksheets_helper_test.rb
+++ test/unit/helpers/worksheets_helper_test.rb
@@ -9,10 +9,17 @@ class WorksheetsHelperTest < ActionView::TestCase
   
   test "problem_group_instructions returns expected text" do
     expected_msg = "Solve the damn problem."
-    mock_prob = MathProblem.new
-    mock_prob.stubs(:instruction_description).returns(expected_msg)
-    prob_group = Array.new(4) {|i| Factory.build(:worksheet_problem, :problem_number => (i + 15), :math_problem => mock_prob, :worksheet => Worksheet.new) }
+    prob_group = problem_group_numbered((15..18))
+    prob_group.each {|wp| wp.math_problem.stubs(:instruction_description).returns(expected_msg)}
+    
     expected_message = "Problems #15, 16, 17, 18: Solve the damn problem."
     assert_equal expected_message, problem_group_instructions(prob_group)
   end
+
+  private
+  def problem_group_numbered(number_range = (1..1))
+    first_num = number_range.first
+    Array.new(number_range.count) {|i| Factory.build(:worksheet_problem, :problem_number => (i + first_num), :math_problem => MathProblem.new, :worksheet => Worksheet.new) }
+  end
+  
 end
