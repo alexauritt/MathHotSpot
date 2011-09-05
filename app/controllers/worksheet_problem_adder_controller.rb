fba7ee86598c61578afab6dcc5289ad2f211a9c4
Alexander Auritt <alexauritt@gmail.com>
2011-09-05 11:55:41 -0400

    wpadder#create delegates to unwritten worksheet#add_problem_like

--- app/controllers/worksheet_problem_adder_controller.rb
+++ app/controllers/worksheet_problem_adder_controller.rb
@@ -1,4 +1,9 @@
 class WorksheetProblemAdderController < ApplicationController
   def create
+    @worksheet = Worksheet.find(params[:worksheet_problem_adder][:worksheet_id])
+    @worksheet.add_problem_like(params[:worksheet_problem_adder][:problem_number].to_i)
+    # problems_of_this_type_on_worksheet = get all math problems from same level as prob above on worksheet
+    # MathProblem.find_problem_like(problem, :exclude => )
+    render :text => "finished"
   end
 end
--- test/functional/worksheet_problem_adder_controller_test.rb
+++ test/functional/worksheet_problem_adder_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
-class WorksheetProblemAdderControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+class WorksheetProblemAdderControllerTest < AuthenticatingControllerTestCase
+  test "create delegates to Worksheet add_problem_like" do
+    worksheet_id = 6666
+    problem_number = 3
+    
+    worksheet = Factory.build(:worksheet)
+    worksheet.expects(:add_problem_like).with(problem_number).returns(true)
+    Worksheet.expects(:find).with(worksheet_id).returns(worksheet)
+    
+    post :create, :worksheet_problem_adder => {:worksheet_id => worksheet_id, :problem_number => problem_number}
+    
+    assert_response :success
   end
 end
