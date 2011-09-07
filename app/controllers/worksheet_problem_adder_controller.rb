df86d475547bda0bc82dafb38a813a0006557284
Alexander Auritt <alexauritt@gmail.com>
2011-09-07 16:04:14 -0400

    worksheet problem adder handles unique problem request

--- app/controllers/worksheet_problem_adder_controller.rb
+++ app/controllers/worksheet_problem_adder_controller.rb
@@ -1,9 +1,9 @@
 class WorksheetProblemAdderController < ApplicationController
   def create
     @worksheet = Worksheet.find(params[:worksheet_problem_adder][:worksheet_id])
-    @worksheet.add_problem_like!(params[:worksheet_problem_adder][:problem_number].to_i)
-    # problems_of_this_type_on_worksheet = get all math problems from same level as prob above on worksheet
-    # MathProblem.find_problem_like(problem, :exclude => )
-    render :text => "finished"
+    unless @worksheet.add_problem_like!(params[:worksheet_problem_adder][:problem_number].to_i)
+      @worksheet.errors[:base].each { |error| flash.now[:notice] = error }
+    end
+    render :template => "worksheets/show"
   end
 end
--- test/functional/worksheet_problem_adder_controller_test.rb
+++ test/functional/worksheet_problem_adder_controller_test.rb
@@ -1,16 +1,39 @@
 require 'test_helper'
 
 class WorksheetProblemAdderControllerTest < AuthenticatingControllerTestCase
+  include RightRabbitErrors
+  def setup
+    @worksheet = Factory.build(:worksheet)
+    @worksheet.stubs(:to_param).returns('23') # random id so worksheet is routable
+    @new_worksheet_problem = Factory.build(:worksheet_problem, :worksheet => @worksheet)
+    @worksheet_id = 6666
+    @problem_number = 3    
+    
+  end
   test "create delegates to Worksheet add_problem_like" do
-    worksheet_id = 6666
-    problem_number = 3
+
+    @worksheet.expects(:add_problem_like!).with(@problem_number).returns(@new_worksheet_problem)
+    Worksheet.expects(:find).with(@worksheet_id).returns(@worksheet)
     
-    worksheet = Factory.build(:worksheet)
-    worksheet.expects(:add_problem_like!).with(problem_number).returns(true)
-    Worksheet.expects(:find).with(worksheet_id).returns(worksheet)
-    
-    post :create, :worksheet_problem_adder => {:worksheet_id => worksheet_id, :problem_number => problem_number}
-    
+    post :create, :worksheet_problem_adder => {:worksheet_id => @worksheet_id, :problem_number => @problem_number}
+    assert_template "worksheets/show"
     assert_response :success
+    assert_error_message false
+  end
+  
+  test "renders worksheet with error if add_problem_like! returns nil and adds base error to worksheet" do
+    @worksheet.errors[:base] = UNIQUE_PROBLEM_ERROR
+    @worksheet.expects(:add_problem_like!).with(@problem_number).returns(nil)
+    Worksheet.expects(:find).with(@worksheet_id).returns(@worksheet)
+        
+    post :create, :worksheet_problem_adder => {:worksheet_id => @worksheet_id, :problem_number => @problem_number}
+    assert_response :success
+    assert_template "worksheets/show"
+    assert_select 'div.notice', UNIQUE_PROBLEM_ERROR
+  end
+  
+  private
+  def assert_error_message(bool = true)
+    assert_select 'div.notice', bool
   end
 end
