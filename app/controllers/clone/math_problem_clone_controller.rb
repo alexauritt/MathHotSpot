7aa3d6dcd8132dff23efb2ad970a9dbec727bdf6
Alexander Auritt <alexauritt@gmail.com>
2011-09-11 09:06:09 -0400

    abstracted current_level partial for math_problem#new

--- app/controllers/clone/math_problem_clone_controller.rb
+++ app/controllers/clone/math_problem_clone_controller.rb
@@ -2,7 +2,6 @@ class Clone::MathProblemCloneController < ApplicationController
   def new
     parent = MathProblem.find(params[:math_problem_id])
     @problem_type = parent.problem_type
-    @problem_levels = @problem_type.problem_levels
     @math_problem = parent.clone
     @siblings = parent.siblings(10)
     render "math_problems/new"
