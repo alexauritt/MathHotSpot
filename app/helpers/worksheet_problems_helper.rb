23524a8dc534a7173a6b799acaf86c278fb193b9
Alexander Auritt <alexauritt@gmail.com>
2011-10-05 13:19:01 -0400

    moved numerous problem link button partials to subfolder in worksheet_problems view directory

--- app/helpers/worksheet_problems_helper.rb
+++ app/helpers/worksheet_problems_helper.rb
@@ -2,15 +2,15 @@ module WorksheetProblemsHelper
   def add_replace_or_create_similar_worksheet_problem_for(worksheet_problem)
     locals = { :worksheet_problem => worksheet_problem }
     if worksheet_problem.replacement_available?
-      render :partial => "worksheet_problems/replace_and_add_similar_worksheet_problem_buttons", :locals => locals
+      render :partial => "worksheet_problems/problem_links/replace_and_add_similar_worksheet_problem_buttons", :locals => locals
     else
-      render :partial => 'worksheet_problems/create_similar_problem', :locals => locals
+      render :partial => 'worksheet_problems/problem_links/create_similar_problem', :locals => locals
     end
   end
   
   def display_level_link_if_classified!(worksheet_problem)
     if worksheet_problem.classified?
-      render :partial => "worksheet_problems/level_link", :locals => { :worksheet_problem => worksheet_problem }
+      render :partial => "worksheet_problems/problem_links/level_link", :locals => { :worksheet_problem => worksheet_problem }
     end
   end
   
