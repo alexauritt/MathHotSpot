16c0f8e773afd4b7c152508bf4ea308004a68efa
Alexander Auritt <alexauritt@gmail.com>
2011-10-09 08:57:50 -0400

    bug fix: MPCloneControl#new was steering towards UPDATE of parent math prob, not creation of clone.

--- app/controllers/clone/math_problem_clone_controller.rb
+++ app/controllers/clone/math_problem_clone_controller.rb
@@ -2,7 +2,10 @@ class Clone::MathProblemCloneController < ApplicationController
   def new
     parent = MathProblem.find(params[:math_problem_id])
     @problem_type = parent.problem_type
-    @math_problem = parent.clone
+    
+    new_prob_attrs = {:problem_level => parent.problem_level, :question_markup => parent.question_markup, :answer_markup => parent.answer_markup}
+    @math_problem = MathProblem.new new_prob_attrs
+
     @siblings = parent.siblings(10)
     render "math_problems/new"
   end
--- test/functional/clone/math_problem_clone_controller_test.rb
+++ test/functional/clone/math_problem_clone_controller_test.rb
@@ -4,7 +4,9 @@ class Clone::MathProblemCloneControllerTest < AuthenticatingControllerTestCase
   test "new" do
     parent = math_problems(:simple_monomial_problem_04)
     get :new, :math_problem_id => parent.id
+
     assert_not_nil assigns(:math_problem)
     assert_response :success
+    assert_select "input[value='Create Math problem']"
   end
 end
